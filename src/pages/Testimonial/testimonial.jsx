573577e20a1296193da9ddc5372957070fc8767d
Kavi-R05 <kaviyanagavalli05@gmail.com>
2025-08-12 14:15:42 +0530

    fixing push

--- src/pages/Testimonial/testimonial.jsx
+++ src/pages/Testimonial/testimonial.jsx
@@ -1,31 +1,70 @@
-import React, { useEffect } from 'react'
-import { show, testimonialGetAll } from '../../store/slice/testimonailSlice'
-import { useDispatch, useSelector } from 'react-redux'
-import CustomTable from '../../components/table/Table'
-
-
+import React, { useEffect } from "react";
+import { show, testimonialGetAll } from "../../store/slice/testimonailSlice";
+import { useDispatch, useSelector } from "react-redux";
+import CustomTable from "../../components/table/Table";
+import { Button, Space } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
 const Testimonial = () => {
-    //   const useselector = useSelector((state) => state.com);
+  //   const useselector = useSelector((state) => state.com);
 
-    const testimonialSelector =  useSelector((state)=> state.testimonial) 
-    
-    const dispatch = useDispatch();
-    useEffect(() => {
-      dispatch(testimonialGetAll())
-    },[])
-
-    // const data = [{name: "aaa", age:"bbb"}]
-    const columns = [
-      { title: 'Name', dataIndex: 'name' },
-      { title: 'Rating', dataIndex: 'retting' },
-      { title: 'createdAt', dataIndex: 'createdAt' },
-      { title: 'Action', dataIndex: 'action' },
-    ]
+  const testimonialSelector = useSelector((state) => state.testimonial);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(testimonialGetAll());
+  }, []);
+  const handleEdit = (record) => {
+    navigate(`dashboard/edit-testimonial/${record.id}`);
+  };
+  // const data = [{name: "aaa", age:"bbb"}]
+  const columns = [
+    { title: "Name", dataIndex: "name" },
+    { title: "Rating", dataIndex: "retting" },
+    {
+      title: "createdAt",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (text) => {
+        if (!text) return "";
+        const date = new Date(text);
+        const day = String(date.getDate()).padStart(2, "0");
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const year = date.getFullYear();
+        return `${day}-${month}-${year}`;
+      },
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      key: "action",
+      render: (_, record) => (
+        <Space>
+          <Button
+            type="primary"
+            icon={<EditOutlined />}
+            onClick={() => handleEdit(record)}
+          ></Button>
+          <Button
+            type="danger"
+            icon={<DeleteOutlined />}
+            onClick={() => console.log("Delete", record)}
+          ></Button>
+        </Space>
+      ),
+    },
+  ];
 
   return (
-    <CustomTable columns={columns} data={testimonialSelector.testimonialTableData} />
-  )
-}
+    <CustomTable
+      columns={columns}
+      data={testimonialSelector.testimonialTableData.map((row) => ({
+        ...row,
+        key: row.id, // or row._id, must be unique
+      }))}
+    />
+  );
+};
 
-export default Testimonial
+export default Testimonial;
